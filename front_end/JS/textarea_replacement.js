21e02d0da5ef54bc25366be94ed8af6223770751
Chesed Casey Collins <chesedcaseycollins@gmail.com>
2023-09-21 11:02:25 -0400

    found a fix for textarea height issue

---
+++ front_end/JS/textarea_replacement.js
@@ -0,0 +1,18 @@
+// This script creates a div to replace the default text area because it is easier to edit
+
+let textarea_replacement = document.createElement("div");
+textarea_replacement.classList = "textarea_replacement"
+textarea_replacement.hidden = true;
+let button_for_reference = document.querySelector("button");
+let parent = document.querySelector("form");
+
+parent.insertBefore(textarea_replacement, button_for_reference);
+
+let textarea = document.querySelector(".textarea_default");
+let div = document.querySelector(".textarea_replacement");
+textarea.hidden = true;
+div.hidden = false;
+div.contentEditable = "true";
+div.oninput = (e) => {
+    textarea.value = div.innerHTML;
+};
