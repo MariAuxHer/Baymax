df00c462eb80ce9855290f1c103bf6144ed466fd
Steven Dao <stevendao100@gmail.com>
2023-10-04 10:04:01 -0400

    removing the env folder from the repo and added a another gitignore to make sure it doesn't get pushed again

--- back_end/env/bin/activate.fish
+++ back_end/env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/nevets/code/Baymax/env"
+set -gx VIRTUAL_ENV "/home/nevets/code/Baymax/back_end/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
