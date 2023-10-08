5a431a7a44dd037e9b62f4c21f88934451464770
Steven Dao <stevendao100@gmail.com>
2023-10-07 22:39:28 -0500

    a

--- front_end/JS/server.js
+++ front_end/JS/server.js
@@ -1,15 +1,4 @@
-getCSRF = () => {
-    fetch("/api/csrf/", {
-      credentials: "same-origin",
-    })
-    .then((res) => {
-      let csrfToken = res.headers.get("X-CSRFToken");
-      console.log(csrfToken);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  }
+console.log("hello world")
 
 
-getCSRF()
+while (true) {}
