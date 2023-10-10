d12fef5b9af6a45246342c47347a99328590546c
Jonathan Tran <jonathan.tran227@gmail.com>
2023-10-10 03:03:01 -0400

    initial commit for login and sign up pages, tweaked some directories in index.html

---
+++ front_end/JS/login.js
@@ -0,0 +1,7 @@
+document.getElementById('toLoginPage').addEventListener('click', function() {
+    window.location.href = 'http://127.0.0.1:5500/login.html?';
+});
+
+// document.getElementById('header1').addEventListener('click', function() {
+//     window.location.href = 'http://127.0.0.1:5500/index.html';
+// });
