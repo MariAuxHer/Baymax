e1b98e09be5eefe758319f16e1d86f4ca64caacc
amals610 <100894287+amals610@users.noreply.github.com>
2023-10-12 12:48:55 -0400

    Update login.js
    testing sum

--- front_end/JS/login.js
+++ front_end/JS/login.js
@@ -1,5 +1,9 @@
-document.getElementById('toLoginPage').addEventListener('click', function() {
+/* document.getElementById('toLoginPage').addEventListener('click', function() {
     window.location.href = 'http://127.0.0.1:5500/login.html?';
+}); */
+
+document.getElementById('toLoginPage').addEventListener('click', function() {
+    window.location.href = 'login.html';
 });
 
 // document.getElementById('header1').addEventListener('click', function() {
