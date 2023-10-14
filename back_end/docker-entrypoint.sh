5c120e5d4aa348a451d0801edc49441fa53285cc
Steven Dao <stevendao100@gmail.com>
2023-10-14 03:14:38 -0400

    create the entry point for backend docker

--- back_end/docker-entrypoint.sh
+++ back_end/docker-entrypoint.sh
@@ -2,7 +2,13 @@
 
 # Apply database migrations
 echo "Apply database migrations"
-python manage.py migrate
+python3.11 manage.py migrate
+
+# create test user
+echo "Creating sample user!"
+echo "username: 'test'"
+echo "password: 'test'"
+printf 'test\n \n test\n test\n y\n' | python3.11 manage.py createsuperuser
 
 # Start server
 echo "Starting server"
