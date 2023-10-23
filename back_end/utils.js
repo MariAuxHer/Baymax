f621bf038b7529c16f6c549f61f30c714f164a62
Steven Dao <stevendao100@gmail.com>
2023-10-22 20:30:21 -0400

    modified the utils file

--- back_end/utils.js
+++ back_end/utils.js
@@ -18,7 +18,17 @@ let logged_in = false
     Returns the Json after calling the CSRF_URL for a CSRF Token
 */
 async function fetch_csrf() {
-    const response = await fetch(CSRF_URL);
+    header = {
+        "Content-Type": "application/json",
+    }
+
+    if (cookies['sessionid']) {
+        header.sessionid = cookies['sessionid']
+    }
+
+    const response = await fetch(CSRF_URL, {
+        headers: header
+    });
 
     if (!response.ok) {
         response.text().then((data) => {
@@ -31,11 +41,10 @@ async function fetch_csrf() {
             }
         })
 
-        return
+        return 
     }
 
-    cookies['csrftoken'] = response.headers.get("X-CSRFToken")
-    console.log("FETCH_CSRF set csrfToken to: " + cookies['csrftoken'])
+    return response.headers.get("X-CSRFToken")
 }
 
 async function login(username, password) {
@@ -106,7 +115,7 @@ async function logout() {
     const response = await fetch(LOGOUT_URL, {
         method: "GET",
         headers: {
-            "Cookie": `csrftoken=${cookies['csrftoken']};sessionid=${cookies['sessionid']}`,
+            "Cookie": `sessionid=${cookies['sessionid']}`,
             "Content-Type": "application/json",
         }
     })
@@ -145,7 +154,7 @@ async function whoami() {
     const response = await fetch(WHOAMI_URL, {
         method: "GET",
         headers: {
-            "Cookie": `csrftoken=${cookies['csrftoken']};sessionid=${cookies['sessionid']}`,
+            "Cookie": `sessionid=${cookies['sessionid']}`,
             "Content-Type": "application/json"
         }
       })
@@ -180,7 +189,7 @@ async function session() {
     const response = await fetch(SESSION_URL, {
         method: "GET",
         headers: {
-            "Cookie": `csrftoken=${cookies['csrftoken']};sessionid=${cookies['sessionid']}`,
+            "Cookie": `sessionid=${cookies['sessionid']}`,
             "Content-Type": "application/json"
         }
     })
@@ -214,7 +223,7 @@ async function get_conversations() {
     const response = await fetch (CONVERSATIONS_URL, {
         method: "GET",
         headers: {
-            "Cookie": `csrftoken=${cookies['csrftoken']};sessionid=${cookies['sessionid']}`,
+            "Cookie": `sessionid=${cookies['sessionid']}`,
             "Content-Type": "application/json"
         }
     })
@@ -239,17 +248,57 @@ async function get_conversations() {
     console.log("GET_CONVERSATIONS End")
 }
 
+async function post_conversation(name) {
+    console.log("POST_CONVERSATION Start")
+
+    const response = await fetch(CONVERSATIONS_URL + "/", {
+        method: "POST",
+        headers: {
+            "Cookie": `csrftoken=${cookies['csrftoken']};sessionid=${cookies['sessionid']}`,
+            "X-CSRFToken": cookies['csrftoken'],
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            name: name
+        })
+    })
+
+    if (response.ok) {
+        console.log("CONVERSATIONS POST Response OK")
+        const json = await response.json()
+        console.log(JSON.stringify(json))
+    } else {
+        console.log("CONVERSATIONS POST Response NOT OK")
+        response.text().then((data) => {
+            if (data) {
+                fs.writeFile("conversationsPOST_html.html", data, (err) => {
+                    if (err) console.log(`error: ${err}`)
+                })
+            } else {
+                console.log("data is null")
+            }
+        })
+    }
+
+
+    console.log("POST_CONVERSATION End")
+}
+
 // Main
 
 async function main() {
     fetch_csrf()
+    .then( (data) => {
+        cookies['csrftoken'] = data
+    })
     .then(() => login("test", "test"))
     .then(() => whoami())
     .then(() => session())
     .then(() => get_conversations())
+    .then(() => fetch_csrf())
+    .then(() => post_conversation("data one two"))
     .then(() => logout())   
 }
-
 
 main(); 
 
