f97807175a6becacc890cde2976e02449929cad9
Steven Dao <stevendao100@gmail.com>
2023-10-29 23:09:45 -0400

    docker files should be set up to track front and back end and a test user is auto setup on server startup

--- back_end/docker-entrypoint.sh
+++ back_end/docker-entrypoint.sh
@@ -6,7 +6,13 @@ python3.11 manage.py makemigrations back_end
 python3.11 manage.py migrate
 
 # collect static # files will be used by nginx
+echo "Generating Django's static files" 
 python3.11 manage.py collectstatic --noinput
+
+# auto create a test user
+echo "Creating a test user with username: "test" and password: "test"" 
+export DJANGO_SUPERUSER_PASSWORD="test"
+python3.11 manage.py createsuperuser --username "test" --email "test@gmail.com" --noinput 
 
 # Start server
 echo "Starting server"
