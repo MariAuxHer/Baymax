1f35cb61fcd66337190f9af6ab6fba65910ae606
amals610 <amalanto.sam@gmail.com>
2023-10-31 03:46:07 -0400

    fixing about buttons

---
+++ front_end/JS/about.js
@@ -0,0 +1,14 @@
+// Redirect to Login Page
+document.getElementById('toLoginPage').addEventListener('click', function() {
+    window.location.href = 'login.html';
+});
+
+// Redirect to About Page
+document.getElementById('toAboutPage').addEventListener('click', function() {
+    window.location.href = 'about.html';
+});
+
+// Redirect to Profile Page
+document.getElementById('toProfilePage').addEventListener('click', function() {
+    window.location.href = 'profile.html';
+});
