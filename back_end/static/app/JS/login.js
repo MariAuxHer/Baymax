885d80fa80a41750b3c4a4a01f23e121169a5903
Chesed Casey Collins <chesedcaseycollins@gmail.com>
2023-11-13 15:22:49 -0500

    catching up

--- back_end/static/app/JS/login.js
+++ back_end/static/app/JS/login.js
@@ -21,13 +21,6 @@ document.getElementById('submit').addEventListener('click', async function(event
         // let error_text = document.getElementById("error");
         // error_text.textContent = JSON.parse(result.detail);
         
-<<<<<<< HEAD
-        let parent = document.querySelector("#login_form");
-        let paragraph = document.createElement("p");
-        paragraph.classList.add("form_fail");
-        paragraph.innerHTML = "The username and/or password you entered does not match our records.";
-        parent.appendChild(paragraph);
-=======
         let exists = document.querySelector(".form_fail");
         if (!exists) {
             let parent = document.querySelector("#login_form");
@@ -36,7 +29,6 @@ document.getElementById('submit').addEventListener('click', async function(event
             paragraph.innerHTML = "The username and/or password you entered does not match our records.";
             parent.appendChild(paragraph);
         }
->>>>>>> 0ae0b896dfc54b30b4ce971d9be775ffb36b30e4
 
     }
 
