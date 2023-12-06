978896048ec5af13c1362e2788dff98446123ffc
Jordan Huff <jordantrhuff@gmail.com>
2023-12-05 19:22:50 -0500

    attempt 8

--- back_end/back_end/openai_interaction.py
+++ back_end/back_end/openai_interaction.py
@@ -26,6 +26,10 @@ def custom_standardization(input_data):
                                   '[%s]' % re.escape(string.punctuation),
                                   '')
 
+
+vocab = ['', '[UNK]', 'the', 'of', 'can', 'my', 'how', 'you', 'what', 'i', 'for', 'and', 'is', 'on', 'are', 'in', 'foot', 'skin', 'or', 'eye', 'recommend', 'to', 'health', 'eyes', 'between', 'impact', 'a', 'does', 'there', 'neurological', 'explain', 'care', 'during', 'treatments', 'effects', 'disorders', 'specific', 'pain', 'address', 'changes', 'hormonal', 'use', 'manage', 'way', 'best', 'potential', 'have', 'from', 'if', 'about', 'prevent', 'connection', 'concerns', 'provide', 'do', 'diagnosed', 'managing', 'affect', 'role', 'caused', 'by', 'information', 'certain', 'system', 'protect', 'nervous', 'conditions', 'ball', 'any', 'with', 'treated', 'should', 'relationship', 'exercises', 'feet', 'condition', 'causes', 'exposure', 'discuss', 'link', 'vision', 'signs', 'problems', 'function', 'considerations', 'people', 'managed', 'it', 'alleviate', 'acne', 'when', 'ways', 'that', 'reducing', 'persistent', 'infections', 'concept', 'autoimmune', 'appearance', 'tips', 'symptoms', 'risk', 'prolonged', 'improving', 'especially', 'blisters', 'sleep', 'products', 'discomfort', 'different', 'significance', 'pregnancy', 'medications', 'injuries', 'developing', 'chronic', 'as', 'abscess', 'toe', 'such', 'skincare', 'screens', 'redness', 'overthecounter', 'infection', 'hyperpigmentation', 'hair', 'excessive', 'diabetes', 'viral', 'type', 'thyroid', 'stress', 'removing', 'obesity', 'may', 'history', 'friction', 'diseases', 'disease', 'development', 'dehydration', 'contribute', 'contraceptives', 'caffeine', 'be', 'avoid', 'allergens', 'adolescence', 'wearing', 'treatment', 'treat', 'toes', 'toenails', 'shoes', 'scars', 'peripheral', 'neurodevelopmental', 'neurodegenerative', 'makeup', 'lifestyle', 'inflammatory', 'help', 'fungal', 'factors', 'extended', 'environment', 'drops', 'current', 'cosmetics', 'bloodshot', 'at', 'ankle', 'age', 'syndrome', 'sweating', 'structure', 'strain', 'spots', 'smartphones', 'sides', 'retinopathy', 'resolving', 'puberty', 'psoriasis', 'pressure', 'plantar', 'perimenopause', 'night', 'neuropathy', 'loss', 'like', 'lenses', 'ingrown', 'improve', 'high', 'headlights', 'glare', 'genetics', 'fracture', 'environmental', 'driving', 'draining', 'digital', 'diabetic', 'dermatitis', 'contact', 'computer', 'burning', 'bottom', 'weather', 'tumors', 'toenail', 'tight', 'them', 'swelling', 'sun', 'sports', 'routine', 'remedies', 'proper', 'preventing', 'play', 'physical', 'movement', 'menopause', 'long', 'lipomas', 'ingredients', 'highimpact', 'heels', 'harsh', 'growth', 'flexibility', 'fatty', 'epilepsy', 'eczema', 'difference', 'diet', 'dark', 'damage', 'cysts', 'circulation', 'cancer', 'brain', 'boil', 'arch', 'antiperspirants', 'alopecia', 'alcohol', 'activities', 'women', 'weightbearing', 'warts', 'warning', 'walking', 'their', 'take', 'swimming', 'suggest', 'sudden', 'stretches', 'stretch', 'strength', 'sleeping', 'side', 'sensitive', 'seek', 'scratched', 'sclerosis', 'rosacea', 'right', 'pools', 'polyneuropathy', 'pattern', 'other', 'neuropathies', 'neurogenetic', 'multiple', 'migraines', 'menstruation', 'marks', 'maintaining', 'look', 'lack', 'joints', 'issues', 'influence', 'hyperhidrosis', 'heel', 'harmful', 'guidance', 'gritty', 'footwear', 'feeling', 'dryness', 'dietary', 'devices', 'demyelinating', 'cornea', 'common', 'choose', 'chlorine', 'childrens', 'children', 'central', 'calluses', 'breakouts', 'body', 'blurred', 'balance', 'arches', 'antifungal', 'an', 'aging', 'achilles', 'wrinkles', 'will', 'whiteheads', 'weakness', 'vitiligo', 'verrucas', 'under', 'types', 'tumor', 'triggers', 'treating', 'training', 'toxins', 'tingling', 'therapy', 'tension', 'tendon', 'tags', 'systemic', 'surgery', 'sunscreen', 'suitable', 'strengthening', 'strengthen', 'strategies', 'stabbing', 'soles', 'smoking', 'shingles', 'seizures', 'seborrheic', 'sebaceous', 'seasons', 'running', 'risks', 'retinas', 'restless', 'related', 'rehabilitation', 'recreational', 'puncture', 'puffiness', 'poor', 'pollution', 'pigmentation', 'pesticides', 'parkinsons', 'palmar', 'options', 'oily', 'often', 'numbness', 'normal', 'nonsurgical', 'neurotransmitters', 'neurology', 'neuroinflammation', 'nerve', 'natural', 'nail', 'myasthenia', 'muscles', 'milia', 'metatarsal', 'metals', 'metabolic', 'memory', 'melasma', 'mechanics', 'management', 'longterm', 'lipoma', 'linked', 'legs', 'keloids', 'itching', 'injury', 'inherited', 'importance', 'hypertrophic', 'home', 'highaltitude', 'height', 'healthy', 'gravis', 'fungus', 'flushing', 'flareups', 'firmness', 'fetal', 'fasciitis', 'face', 'environments', 'elliptical', 'elasticity', 'early', 'dry', 'drugs', 'diagnosis', 'cystic', 'coordination', 'context', 'concerned', 'complications', 'color', 'circles', 'childs', 'cause', 'calf', 'bunions', 'breastfeeding', 'bloodbrain', 'blood', 'blister', 'benefits', 'barrier', 'bacterial', 'axis', 'aware', 'athletes', 'arthritis', 'apnea', 'antiviral', 'antiaging', 'ankles', 'alzheimers', 'allergic', 'alignment', 'wound', 'worsen', 'workplace', 'without', 'within', 'withdrawal', 'wellbeing', 'weight', 'weak', 'vestibular', 'versicolor', 'veins', 'using', 'untreated', 'uneven', 'undereye', 'ulcers', 'trim', 'trigger', 'trigeminal', 'tremors', 'top', 'tone', 'tinea', 'time', 'tightening', 'they', 'texture', 'tendonitis', 'tailored', 'support', 'sunspots', 'sunburn', 'stroke', 'stiffness', 'standing', 'stages', 'stability', 'spurs', 'sprained', 'splints', 'spider', 'socks', 'shoe', 'shin', 'sesamoid', 'sensitization', 'sensations', 'selfexaminations', 'see', 'schedule', 'scan', 'sagging', 'runners', 'rule', 'rough', 'ringworm', 'rhythm', 'rheumatoid', 'replacing', 'relieving', 'relieve', 'regular', 'reduce', 'recovery', 'recommended', 'recommendations', 'reading', 'reactions', 'reaction', 'rashes', 'rash', 'range', 'radiation', 'pulses', 'protein', 'protecting', 'progression', 'progress', 'procedures', 'problem', 'prescription', 'prescribed', 'premature', 'pregnant', 'precautions', 'posture', 'postinflammatory', 'plasticity', 'plan', 'pilaris', 'periods', 'performed', 'perform', 'pediatric', 'patients', 'patches', 'palsy', 'palms', 'outside', 'osteoporosis', 'orthotics', 'orthopedic', 'odor', 'nutritional', 'nutrition', 'nonprescription', 'noise', 'neurotrophins', 'neurotoxicity', 'neurostimulation', 'neuroprotection', 'neuroplasticity', 'neuropharmacology', 'neuropathic', 'neuronal', 'neuromuscular', 'neurologist', 'neuroimaging', 'neurogenesis', 'neurofeedback', 'neuroethics', 'neuroendocrine', 'neuralgia', 'need', 'necessary', 'near', 'nails', 'mycotoxins', 'mutations', 'muscle', 'mri', 'motion', 'morning', 'mood', 'monitor', 'moles', 'mold', 'moisturizer', 'moisture', 'modifications', 'mobility', 'misfolding', 'migraine', 'microbiota', 'microbiomegutbrain', 'mercury', 'melanoma', 'medication', 'manifest', 'make', 'lupus', 'lumbar', 'loose', 'localized', 'lines', 'life', 'lead', 'latest', 'know', 'keratosis', 'its', 'issue', 'irritation', 'involved', 'instability', 'inspections', 'inserts', 'inflammation', 'inflamed', 'increase', 'improvements', 'implications', 'impetigo', 'imbalances', 'imaging', 'illfitting', 'ignoring', 'identifying', 'hypersensitivity', 'hyperactivity', 'hygiene', 'hydration', 'huntingtons', 'hormones', 'hirsutism', 'hiking', 'herpes', 'hereditary', 'herbicides', 'heavy', 'headache', 'head', 'hammertoes', 'habit', 'gutbrain', 'gut', 'guillainbarré', 'groin', 'gout', 'get', 'gentle', 'genetic', 'future', 'functional', 'frequency', 'followup', 'folliculitis', 'follicles', 'flat', 'flareup', 'fine', 'field', 'fibromyalgia', 'fatigue', 'family', 'families', 'falls', 'exercise', 'exam', 'exacerbating', 'exacerbates', 'endocannabinoid', 'encephalomyelitis', 'encephalitis', 'electronic', 'electromagnetic', 'effectively', 'effective', 'dystonia', 'distances', 'disseminated', 'disorder', 'differentiate', 'diagnose', 'detection', 'deprivation', 'depression', 'dehydrated', 'deformities', 'deficit', 'deficiencies', 'cycling', 'cut', 'custom', 'ct', 'creams', 'cramps', 'cosmetic', 'correlation', 'corns', 'consolidation', 'conducted', 'complement', 'cognitive', 'cleanser', 'circadian', 'choice', 'chemicals', 'checking', 'check', 'cerebral', 'cellulitis', 'causing', 'capillaries', 'bunion', 'bromhidrosis', 'broken', 'bones', 'biofeedback', 'benefit', 'based', 'bags', 'available', 'autonomic', 'auras', 'attention', 'athletic', 'associated', 'artery', 'armpits', 'areas', 'approach', 'appointments', 'applications', 'antipsychotic', 'anticonvulsant', 'anesthesia', 'als', 'allergies', 'after', 'affecting', 'aerobics', 'advancements', 'adults', 'adolescents', 'adhd', 'addressing', 'acute', 'activity', 'abrasion', 'abnormalities', 'abcde']
+
+
 max_features = 10000
 sequence_length = 250
 
@@ -33,7 +37,8 @@ vectorize_layer = layers.TextVectorization(
     standardize=custom_standardization,
     max_tokens=max_features,
     output_mode='int',
-    output_sequence_length=sequence_length)
+    output_sequence_length=sequence_length,
+    vocabulary=vocab)
 
 export_model = tf.keras.Sequential([
   vectorize_layer,
@@ -142,14 +147,9 @@ def generate_llm_response(prompt, user_city):
             return response 
         
         else:
-            print("TYPEOF")
-            print(type(prompt))
             classification_prompt = [prompt]
-            print(classification_prompt)
-            print(type(classification_prompt))
             classification_results = export_model.predict(classification_prompt)
 
-            classification_success = True
             classification = ""
 
             max_val = max(classification_results[0])
@@ -162,15 +162,15 @@ def generate_llm_response(prompt, user_city):
                 best_guess = -1
                 for i in range(0, len(classification_results[0])):
                     if classification_results[0][i] == max_val:
-                        buest_guess = i
+                        best_guess = i
 
-                if buest_guess == 0:
+                if best_guess == 0:
                     classification = "dermatologist"
-                elif buest_guess == 1:
+                elif best_guess == 1:
                     classification = "neurologist"
-                elif buest_guess == 2:
+                elif best_guess == 2:
                     classification = "optometrist"
-                elif buest_guess == 3:
+                elif best_guess == 3:
                     classification = "podiatrist"
                 
                 response = gptresponse + f"\nFor more information, find a {classification} in your region\n"
