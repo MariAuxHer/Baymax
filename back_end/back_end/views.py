b68bc46d73f3d24247d7e494170f91cfca33d210
vpkonjeti <vinnikonjeti@gmail.com>
2023-12-06 00:32:18 -0500

    standard response form button

--- back_end/back_end/views.py
+++ back_end/back_end/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
+from django.http import JsonResponse, HttpResponseBadRequest
 
 from django.contrib.auth import logout
 
+from django.views.decorators.csrf import csrf_exempt, csrf_protect
+from django.views.decorators.http import require_http_methods
+
 # database
 from back_end.serializers import ConversationSerializer, UserSerializer, InteractionSerializer, MinimalConversationSerializer
-from back_end.models import Conversation, Interaction, CustomUser
+from back_end.models import Conversation, Interaction, CustomUser, Doctor
+
+import json
 
 def index(request):
 
@@ -52,12 +58,40 @@ def about(request):
 def test(request):
     return render(request, "back_end/test.html")
 
-def search(request):
-    return render(request, "back_end/search.html")
-
 def signout(request):
     logout(request)
     return redirect('/login')
 
 def changeprofile(request):
     return render(request, "back_end/changeprofile.html")
+
+@csrf_protect
+def search(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            selected_city = data['city'].capitalize()  # Capitalize the city name as stored in the database
+            selected_specialization = data['specialization']
+
+            # Filter doctors by city (case-insensitive) and specialization
+            doctors = Doctor.objects.filter(
+                m_address__icontains=selected_city, 
+                specialty__icontains=selected_specialization
+            )
+
+            # Serialize the data into a list of dictionaries
+            doctor_data = [
+                {"name": doctor.name, "address": doctor.m_address, "specialty": doctor.specialty}
+                for doctor in doctors
+            ]
+
+            # Return the serialized data as JSON
+            return JsonResponse(doctor_data, safe=False)
+
+        except json.JSONDecodeError:
+            return JsonResponse({"error": "Invalid JSON data"}, status=400)
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=500)
+
+    # If not a POST request, just render the search page
+    return render(request, "back_end/search.html")
--- back_end/static/app/JS/search.js
+++ back_end/static/app/JS/search.js
@@ -20,7 +20,66 @@ document.getElementById('state').addEventListener('change', function() {
     if (stateGeonameId) {
         loadCities(stateGeonameId); // Pass the state's geonameId to loadCities function
     } else {
-        document.getElementById('city').innerHTML = '<option value="">Select City/County</option>';
+        document.getElementById('city').innerHTML = 'Select City/County';
     }
 });
+
+// This function gets the value of a cookie by name
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+// The submitForm function with the CSRF token included in the headers
+function submitForm() {
+    // Get the CSRF token from the cookie
+    const csrftoken = getCookie('csrftoken');
+
+    // Get the selected city's text content and the specialization value from the form elements
+    const citySelect = document.getElementById('city');
+    const selectedCity = citySelect.options[citySelect.selectedIndex].text; // Corrected line
+    const selectedSpecialization = document.getElementById('specialization').value;
+
+    // Prepare the search criteria, assuming the city names in the database are stored in a case-insensitive manner
+    const searchCriteria = {
+        city: selectedCity,
+        specialization: selectedSpecialization
+    };
+
+    // Make an AJAX POST request to your local search endpoint
+    console.log("body: ", JSON.stringify(searchCriteria));
+    fetch('/search', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrftoken
+        },
+        body: JSON.stringify(searchCriteria),
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+    })
+    .then(data => {
+        // Handle the search results here
+        console.log(data);
+    })
+    .catch(error => {
+        console.error('Error during fetch:', error);
+    });
+}
+
 window.onload = loadStates(6252001);  // Load the states of US
+window.submitForm = submitForm;
